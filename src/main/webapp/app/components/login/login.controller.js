53535959c8c6ea6b5a520a0dfa9d16286fe557a1
syhan <syhan@wisestone.kr>
2016-06-22 12:00:52 +0900

    login commit

--- src/main/webapp/app/components/login/login.controller.js
+++ src/main/webapp/app/components/login/login.controller.js
@@ -5,10 +5,10 @@
         .module('wmsApp') // 모듈 선언
         .controller('LoginController', LoginController); // 컨트롤러 선언
 
-    LoginController.$inject = ['$rootScope', '$state', '$timeout', 'Auth', '$uibModalInstance'];
+    LoginController.$inject = ['$rootScope', '$state', '$timeout', 'Auth', '$uibModalInstance', '$log'];
     // $inject를 이용하여 의존성을 주입하여 객체들의 의존성을 최소화 시킨다.
 
-    function LoginController ($rootScope, $state, $timeout, Auth, $uibModalInstance) {
+    function LoginController ($rootScope, $state, $timeout, Auth, $uibModalInstance, $log) {
         // Controller란 사용자가 접근한 url에 따라서 사용자에게 요청에 맞는 데이터를 scope에 주입하거나
         // model에 의뢰하여, view에 반영하여 사용자에게 알려준다.
         // 서버 에서 직접 뷰 로 접근하는 일종의 중간 통로로서 필요할 때마다 서버와 클라이언트
@@ -38,6 +38,7 @@
         }
 
         function login (event) { // 사용자가 html에서 로그인시 login function이 호출된다.
+            vm.authenticationError = false;
             event.preventDefault();
             Auth.login({ // Auth service를 이용하여 로그인을 수행한다.
                 username: vm.username,
@@ -61,8 +62,10 @@
                     Auth.resetPreviousState();
                     $state.go(previousState.name, previousState.params);
                 }
-            }).catch(function () {
+            }).catch(function (err) {
+                $log.debug("err : ", err.data.message);
                 vm.authenticationError = true;
+                vm.authenticationErrorMessage = err.data.message;
             });
         }
 
