15389d70e34fa94cb86b37218e70c3412f2d9a03
whjang <whjang@wisestone.kr>
2016-06-22 13:59:06 +0900

    * 누락분 커밋

--- src/test/java/kr/wisestone/wms/web/rest/UserResourceIntTest.java
+++ src/test/java/kr/wisestone/wms/web/rest/UserResourceIntTest.java
@@ -68,7 +68,7 @@ public class UserResourceIntTest {
 
     @Test
     public void testGetExistingUserWithAnEmailLogin() throws Exception {
-        User user = userService.createUserInformation("john.doe@localhost.com", "johndoe", "John", "john.doe@localhost.com", "en-US");
+        User user = userService.createUserInformation("john.doe@localhost.com", "johndoe", "John", "john.doe@localhost.com", "en-US", null, null);
 
         restUserMockMvc.perform(get("/api/users/john.doe@localhost.com")
                 .accept(MediaType.APPLICATION_JSON))
@@ -81,7 +81,7 @@ public class UserResourceIntTest {
 
     @Test
     public void testDeleteExistingUserWithAnEmailLogin() throws Exception {
-        User user = userService.createUserInformation("john.doe@localhost.com", "johndoe", "John", "john.doe@localhost.com", "en-US");
+        User user = userService.createUserInformation("john.doe@localhost.com", "johndoe", "John", "john.doe@localhost.com", "en-US", null, null);
 
         restUserMockMvc.perform(delete("/api/users/john.doe@localhost.com")
                 .accept(MediaType.APPLICATION_JSON))
