b14e21f44c599fda5db78a95f91acc1648b90356
whjang <whjang@wisestone.kr>
2016-06-28 11:39:41 +0900

    * 유사 Task 검색 임시 적용

--- src/main/java/kr/wisestone/wms/web/rest/TaskResource.java
+++ src/main/java/kr/wisestone/wms/web/rest/TaskResource.java
@@ -183,7 +183,7 @@ public class TaskResource {
     public ResponseEntity<List<TaskDTO>> findSimilar(@RequestParam String name) throws URISyntaxException {
 
         SearchQuery query = new NativeSearchQueryBuilder().withQuery(
-                                    fuzzyLikeThisQuery("name").likeText(name).maxQueryTerms(12)
+                                    matchQuery("name", name).boost(2).prefixLength(0).slop(3)
                                 ).build();
 
         List<Task> tasks = elasticsearchTemplate.queryForList(query, Task.class);
