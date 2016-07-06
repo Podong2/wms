a8b8b031d5947d65d8663d0a52d528d7a88a7393
whjang <whjang@wisestone.kr>
2016-07-06 18:13:01 +0900

    * wms-tree 필터 기능 결함 수정

--- src/main/webapp/app/components/wms-tree-directive/wmsTree.js
+++ src/main/webapp/app/components/wms-tree-directive/wmsTree.js
@@ -60,12 +60,7 @@ function wmsTree($log) {
         }],
         link: function (scope, element, attrs) {
 
-            // $(".wmsTreeItem").keypress(function(event) {
-            //     $log.debug(event);
-            // });
-
-            element.bind("keydown keypress", function (event) {
-                $log.debug("event : ", event.keyCode);
+            $(element).find('tree-dnd').bind("keydown keypress", function (event) {
 
                 if(event.keyCode === 40) {
                     scope.my_tree.select_next_node();
