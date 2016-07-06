a8e52248927b4026142869815ff03f2f73142c71
infostore <korman21@gmail.com>
2016-07-06 23:20:18 +0900

    controller -> directive -> directive로 function을 전달할 수 있는 방법이 없다.

--- src/main/webapp/app/components/ng-tags-input/autocomplete.directive.js
+++ src/main/webapp/app/components/ng-tags-input/autocomplete.directive.js
@@ -13,51 +13,25 @@ function autocomplete($document, $log, $compile, findUser, $q) {
 
     return {
         restrict: 'E',
-        required: "ngModel",
         scope: {
             source : '&',
-            ngModel : '=',
-            //displayproperty : '=',
-            multiple : '=',
-            maxresultstoshow : '='
+            tags : '=',
+            displayProperty : '@',
+            multiple : '@',
+            maxResultsToShow : '@'
         },
         replace: false,
         templateUrl : 'app/components/ng-tags-input/tagsInput.html',
-        controller: ['$scope', '$element', '$attrs', '$rootScope', function ($scope, $element, $attrs, $rootScope) {
-            //$scope.test = function (value) {
-            //    return [{name : "test"}];
-            //}
+        controller : ['$scope', function($scope) {
+            $scope.loadData = function(name) {
+                var deferred = $q.defer();
+                findUser.findByName(name).then(function(result){
+                    deferred.resolve(result);
+                }); //user search
+                return deferred.promise;
+            };
         }],
         link: function (scope, element, attrs) {
-            scope.tagList=[];
-            scope.test = function (value) {
-                scope.$apply();
-                var deferred = $q.defer();
-                scope.$parent.loadData(value).then(function (response) {
-                    deferred.resolve(response);
-                });
-                return deferred.promise;
-            }
-
-
-            //scope.model = 'tags'
-            //scope.displayproperty = 'name'
-            //scope.multiple = 'true'
-            //scope.maxresultstoshow = '20'
-            //scope.model = attrs['ngModel'];
-            scope.displayproperty = attrs['displayproperty'];
-            //scope.multiple = attrs['multiple'];
-            //scope.maxresultstoshow = attrs['maxresultstoshow'];
-            //var template = '<tags-input ng-model="'+ scope.model +'" display-property="'+ scope.displayproperty +'" multiple="'+ scope.multiple +'">' +
-            //                '<auto-complete source="test" multiple="'+ scope.multiple +'" max-results-to-show="'+ scope.maxresultstoshow +'"></auto-complete>' +
-            //            '</tags-input>';
-            //var linkFn = $compile(template);
-            //var content = linkFn(scope);
-            //element.append(content);
-
-            //scope.source().then(function (response) {
-            //    scope.tagList = response;
-            //});
         }
     }
 }
