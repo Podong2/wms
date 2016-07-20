247855ab5b3732a538637682a3cbd06c15ca0445
syhan <syhan@wisestone.kr>
2016-07-20 10:36:14 +0900

    가입 변경중, 오류 수정중

--- src/main/webapp/app/account/register/register.controller.js
+++ src/main/webapp/app/account/register/register.controller.js
@@ -6,9 +6,9 @@
         .controller('RegisterController', RegisterController);
 
 
-    RegisterController.$inject = ['$translate', '$timeout', 'Auth', 'LoginService', 'Department', 'Company'];
+    RegisterController.$inject = ['$translate', '$timeout', 'Auth', 'LoginService', 'Department', 'Company', '$state', 'toastr'];
 
-    function RegisterController ($translate, $timeout, Auth, LoginService, Department, Company) {
+    function RegisterController ($translate, $timeout, Auth, LoginService, Department, Company, $state, toastr) {
         var vm = this;
 
         vm.doNotMatch = null;
@@ -35,14 +35,19 @@
 
                 Auth.createAccount(vm.registerAccount).then(function () {
                     vm.success = 'OK';
+                    toastr.success('가입을 성공 하였습니다. 로그인하여 이용하세요.', 'WMS System Message');
+                    $state.go('login');
                 }).catch(function (response) {
                     vm.success = null;
                     if (response.status === 400 && response.data === 'login already in use') {
                         vm.errorUserExists = 'ERROR';
+                        toastr.error('가입을 실패 하였습니다.', 'WMS System Message');
                     } else if (response.status === 400 && response.data === 'e-mail address already in use') {
                         vm.errorEmailExists = 'ERROR';
+                        toastr.error('가입을 실패 하였습니다.', 'WMS System Message');
                     } else {
                         vm.error = 'ERROR';
+                        toastr.error('가입을 실패 하였습니다.', 'WMS System Message');
                     }
                 });
             }
--- src/main/webapp/app/components/wms-input/wmsInput.directive.js
+++ src/main/webapp/app/components/wms-input/wmsInput.directive.js
@@ -18,19 +18,34 @@
         return {
             restrict: 'E',
             required : 'ngModel',
+            scope : {
+                formName : '@',
+                inputName : '@',
+                minlength : '@',
+                maxlength : '@',
+                pattern : '@',
+                requiredText : '@',
+                minText : '@',
+                maxText : '@',
+                patternText : '@',
+                placeholderText : '@',
+                inputType : '@',
+                tooltip : '@',
+                tooltipIcon : '@',
+                ngModel : '=ngModel'
+            },
+            //templateUrl : 'app/components/wms-input/wmsInputTemplate.html',
             replace: false,
             compile: function (element, attrs) {
-                var template = '<div class="form-group">'+
-                                    '<label class="input" for="'+ attrs.inputName +'"><i data-ng-hide="\''+attrs.tooltip+'\' == \'\' || \''+attrs.tooltip+'\' == undefined" class="icon-append fa '+ attrs.tooltipIcon +'"></i>'+
+                var template = '<label class="input" for="'+ attrs.inputName +'"><i data-ng-hide="\''+attrs.tooltip+'\' == \'\' || \''+attrs.tooltip+'\' == undefined" class="icon-append fa '+ attrs.tooltipIcon +'"></i>'+
                                     '<input type="'+ attrs.inputType +'" class="form-control" id="'+ attrs.inputName +'" name="'+ attrs.inputName +'" placeholder="{{\''+ attrs.placeholderText +'\' | translate}}" ' +
                                     'ng-model="'+ attrs.ngModel +'" ng-minlength="'+ attrs.minlength +'" ng-maxlength="'+ attrs.maxlength +'" ng-pattern="'+ attrs.pattern +'" required wms-kr-update>' +
                                     '<b class="tooltip tooltip-top-right" data-ng-hide="\''+attrs.tooltip+'\' == \'\' || \''+attrs.tooltip+'\' == undefined" ><i class="fa txt-color-teal '+ attrs.tooltipIcon +'"></i> '+ attrs.tooltip +'</b></label>'+
-                                    '<div ng-show="'+ attrs.formName +'.'+ attrs.inputName +'.$dirty && '+ attrs.formName +'.'+ attrs.inputName +'.$invalid">' +
-                                        '<p class="help-block" ng-show="'+ attrs.formName +'.'+ attrs.inputName +'.$error.required" translate="'+attrs.requiredText+'"></p>' +
-                                        '<p class="help-block" ng-show="'+ attrs.formName +'.'+ attrs.inputName +'.$error.minlength" translate="'+attrs.minText+'"></p>' +
-                                        '<p class="help-block" ng-show="'+ attrs.formName +'.'+ attrs.inputName +'.$error.maxlength" translate="'+attrs.maxText+'"></p>' +
-                                        '<p class="help-block" ng-show="'+ attrs.formName +'.'+ attrs.inputName +'.$error.pattern" translate="'+attrs.patternText+'"></p>' +
-                                    '</div>' +
+                                '<div ng-show="'+ attrs.formName +'.'+ attrs.inputName +'.$dirty && '+ attrs.formName +'.'+ attrs.inputName +'.$invalid">' +
+                                    '<p class="help-block" ng-show="'+ attrs.formName +'.'+ attrs.inputName +'.$error.required" translate="'+attrs.requiredText+'"></p>' +
+                                    '<p class="help-block" ng-show="'+ attrs.formName +'.'+ attrs.inputName +'.$error.minlength" translate="'+attrs.minText+'"></p>' +
+                                    '<p class="help-block" ng-show="'+ attrs.formName +'.'+ attrs.inputName +'.$error.maxlength" translate="'+attrs.maxText+'"></p>' +
+                                    '<p class="help-block" ng-show="'+ attrs.formName +'.'+ attrs.inputName +'.$error.pattern" translate="'+attrs.patternText+'"></p>' +
                                 '</div>';
                 element.append(template);
 
