35de12b6c56dae4e2c61c708364efc4685a1e6b1
whjang <whjang@wisestone.kr>
2016-07-21 11:04:28 +0900

    * ITS DateUtil 이관

---
+++ src/main/java/kr/wisestone/wms/common/util/DateUtil.java
@@ -0,0 +1,85 @@
+package kr.wisestone.wms.common.util;
+
+import org.apache.commons.lang.time.DateUtils;
+import org.elasticsearch.common.joda.time.DateTime;
+import org.elasticsearch.common.joda.time.Days;
+import org.elasticsearch.common.joda.time.Hours;
+import org.elasticsearch.common.joda.time.Seconds;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class DateUtil {
+    public static Date convertStrToDate(String source) {
+        return convertStrToDate(source, "yyyy-MM-dd HH:mm:ss");
+    }
+
+    public static Date convertStrToDate(String source, String pattern) {
+        return convertStrToDate(source, pattern, Locale.KOREA);
+    }
+
+    public static Date convertStrToDate(String source, String pattern, Locale locale) {
+        Date date = null;
+        SimpleDateFormat sdf = new SimpleDateFormat(pattern, locale);
+        try {
+            date = sdf.parse(source);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        return date;
+    }
+
+    public static String convertDateToYYYYMMDD(Date source) {
+        return convertDateToStr(source, "yyyy-MM-dd");
+    }
+
+    public static String convertDateToHHMMSS(Date source) {
+        return convertDateToStr(source, "HH:mm:ss");
+    }
+
+    public static String convertDateToStr(Date source) {
+        return convertDateToStr(source, "yyyy-MM-dd HH:mm:ss");
+    }
+
+    public static String convertDateToStr(Date source, String pattern) {
+        return convertDateToStr(source, pattern, Locale.KOREA);
+    }
+
+    public static String convertDateToStr(Date source, String pattern, Locale locale) {
+        String date = null;
+        SimpleDateFormat sdf = new SimpleDateFormat(pattern, locale);
+        date = sdf.format(source);
+
+        return date;
+    }
+
+    public static Date addDays(Date date, int amount) {
+        return DateUtils.addDays(date, amount);
+    }
+
+    public static Date addHours(Date date, int amount) {
+        return DateUtils.addHours(date, amount);
+    }
+
+    public static Date addMinutes(Date date, int amount) {
+        return DateUtils.addMinutes(date, amount);
+    }
+
+    public static Integer getDateDiff(Date fromDate, Date toDate) {
+
+        return Days.daysBetween(new DateTime(fromDate), new DateTime(toDate)).getDays();
+    }
+
+    public static Integer getHourDiff(Date fromDate, Date toDate) {
+
+        return Hours.hoursBetween(new DateTime(fromDate), new DateTime(toDate)).getHours();
+    }
+
+    public static Integer getSecondsDiff(Date fromDate, Date toDate) {
+
+        return Seconds.secondsBetween(new DateTime(fromDate), new DateTime(toDate)).getSeconds();
+    }
+}
