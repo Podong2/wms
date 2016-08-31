27ca8e2b52d2dc1a05e3197ce7b1b1e7a9d88b1f
syhan <syhan@wisestone.kr>
2016-08-31 17:00:16 +0900

    아이콘 작업 및 오류 수정

--- src/main/webapp/app/notification/controller/notification-list.controller.js
+++ src/main/webapp/app/notification/controller/notification-list.controller.js
@@ -8,6 +8,7 @@ angular.module('wmsApp')
 notificationListCtrl.$inject=['$scope', 'Code', '$log', 'AlertService', '$rootScope', '$state', '$stateParams', 'toastr', 'Notification', 'ReadUpload'];
         function notificationListCtrl($scope, Code, $log, AlertService, $rootScope, $state, $stateParams, toastr, Notification, ReadUpload) {
             var vm = this;
+            vm.baseUrl = window.location.origin;
             //vm.showDetail = showDetail;
             vm.notificationReadChange = notificationReadChange;
             vm.getReadList = getReadList;
@@ -22,15 +23,18 @@ notificationListCtrl.$inject=['$scope', 'Code', '$log', 'AlertService', '$rootSc
                 { status: true },  // 새로운 알림
                 { status: false }  // 확인한 알림
             ];
+            vm.listType = 'UN_READ';
 
             function getList(number){
                 tabDisplay(number);
+                vm.listType = 'UN_READ';
                 Notification.query({listType : 'UN_READ'}, onSuccess, onError);
             }
             vm.getList(0);
 
             function getReadList(number){
                 tabDisplay(number);
+                vm.listType = 'READ';
                 Notification.query({listType : 'READ'}, onSuccess, onError);
             }
 
@@ -57,8 +61,9 @@ notificationListCtrl.$inject=['$scope', 'Code', '$log', 'AlertService', '$rootSc
             }
 
             function notificationReadChange(id, checkType, index){
-                vm.notifications[index].readYn = true;
+                if(checkType != 'confirm') vm.notifications[index].readYn = true;
                 ReadUpload.put(id, checkType).then(function(){
+                    Notification.query({listType : vm.listType}, onSuccess, onError);
                 });
             }
 
