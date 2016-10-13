e63ae3d4a3736cdf7775ece932fb9047a53f2ee8
whjang <whjang@wisestone.kr>
2016-10-13 16:23:01 +0900

    * 결함수정

--- src/main/java/kr/wisestone/wms/web/rest/form/ProjectFileDeleteForm.java
+++ src/main/java/kr/wisestone/wms/web/rest/form/ProjectFileDeleteForm.java
@@ -1,25 +1,66 @@
 package kr.wisestone.wms.web.rest.form;
 
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 import java.util.ArrayList;
 import java.util.List;
 
-@Data
-@NoArgsConstructor
 public class ProjectFileDeleteForm {
 
     public List<ProjectFileDeleteObject> projectFileDeleteTargets = new ArrayList<>();
 
-    @Data
-    @NoArgsConstructor
-    public class ProjectFileDeleteObject {
+    public ProjectFileDeleteForm() {
+    }
+
+    public ProjectFileDeleteForm(List<ProjectFileDeleteObject> projectFileDeleteTargets) {
+        this.projectFileDeleteTargets = projectFileDeleteTargets;
+    }
+
+    public List<ProjectFileDeleteObject> getProjectFileDeleteTargets() {
+        return projectFileDeleteTargets;
+    }
+
+    public void setProjectFileDeleteTargets(List<ProjectFileDeleteObject> projectFileDeleteTargets) {
+        this.projectFileDeleteTargets = projectFileDeleteTargets;
+    }
+
+    public static class ProjectFileDeleteObject {
 
         private String entityName;
 
         private Long entityId;
 
         private Long attachedFileId;
+
+        public ProjectFileDeleteObject() {
+        }
+
+        public ProjectFileDeleteObject(String entityName, Long entityId, Long attachedFileId) {
+            this.entityName = entityName;
+            this.entityId = entityId;
+            this.attachedFileId = attachedFileId;
+        }
+
+        public String getEntityName() {
+            return entityName;
+        }
+
+        public void setEntityName(String entityName) {
+            this.entityName = entityName;
+        }
+
+        public Long getEntityId() {
+            return entityId;
+        }
+
+        public void setEntityId(Long entityId) {
+            this.entityId = entityId;
+        }
+
+        public Long getAttachedFileId() {
+            return attachedFileId;
+        }
+
+        public void setAttachedFileId(Long attachedFileId) {
+            this.attachedFileId = attachedFileId;
+        }
     }
 }
