d64fe30cd68dc3773febac175d48021ca9d878e2
syhan <syhan@wisestone.kr>
2016-10-18 17:16:18 +0900

    파일 명 오브젝트로 나오는 현상 수정

--- src/main/webapp/app/components/common/controller/editUpload.controller.js
+++ src/main/webapp/app/components/common/controller/editUpload.controller.js
@@ -41,44 +41,16 @@ uploadCtrl.$inject=['$scope', '$upload', '$log', '$filter', 'TaskEdit', '$cookie
 
 
             $log.debug("파일 유형 확인 : " , listFiles);
-            // hsy 파일 업로드 수정
-            var formData = new FormData();
-            $.each(listFiles, function (key, data) {
-                if (!isEmpty(listFiles[key])) {
-                    formData.append("file", data, listFiles[key]);
-                }
+
+            TaskEdit.fileUpload({
+                method: "POST",
+                file: listFiles,
+                fileFormDataName: "file"
+            }).then(function (response) {
+                $log.debug(response);
+                angular.forEach(response.data, function (fileInfo, index) {
+                    $scope.editor.summernote("editor.insertImage", window.location.origin + "/api/attachedFile/" + fileInfo.id);
+                });
             });
-
-            $.ajax({
-                headers: {
-                    Accept: "application/json; text/plain; */*"
-                    ,"X-CSRF-TOKEN": $cookies.get("CSRF-TOKEN")
-                },
-                url: 'api/attachedFile',
-                processData: false,
-                contentType: false,
-                data: formData,
-                type: 'POST',
-                success: function(response){
-                        $log.debug(response);
-                        angular.forEach(response, function (fileInfo, index) {
-                            $scope.editor.summernote("editor.insertImage", window.location.origin + "/api/attachedFile/" + fileInfo.id);
-                        });
-                },
-                error: function(){
-
-                }
-            });
-
-            //TaskEdit.fileUpload({
-            //    method: "POST",
-            //    files: formData,
-            //    fileFormDataName: "file"
-            //}).then(function (response) {
-            //    $log.debug(response);
-            //    //angular.forEach(response.data.attachedFiles, function (fileInfo, index) {
-            //    //    $scope.editor.summernote("editor.insertImage", window.location.origin + "/attachedFile" + fileInfo.path);
-            //    //});
-            //});
         }
     }
