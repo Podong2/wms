2f329b7a2fa3c2885cb5c70b44c8788dcc44d9ae
syhan <syhan@wisestone.kr>
2016-10-24 11:29:11 +0900

    summernote 댓글 버튼 누를 시 mention 기능 수정

--- src/main/webapp/app/components/common/directive/editorMentionWrite.directive.js
+++ src/main/webapp/app/components/common/directive/editorMentionWrite.directive.js
@@ -40,22 +40,38 @@ function taskEditorMentionWrite($compile, $filter, $log, $sce, $timeout) {
             userId : '=userId'
         },
         link : function (scope, element, attrs) {
-            var editorElementBody = element.parents('.task-detail-area').next().find('.note-editable.panel-body');
-            //var summernoteElement = element.parents('.smart-timeline').next().find('#summernote');
+            var editorElementBody = element.parents('.task-detail-area').next().find('.summernote');
+            var summernoteElement = element.parents('.task-detail-area').next().find('.note-editable.panel-body');
+            var editable = element.parents('.task-detail-area').next().find('[contenteditable]')
             element.on("click", function (event) {
-                if(editorElementBody.find('>p').html() == '<br>') {
-                    editorElementBody.find('>p').prepend('<span class="mentionUser" id="'+ scope.userId +'">@'+ scope.userName +'</span>&nbsp;');
-                    editorElementBody.focus()
+                //editorElementBody.summernote('focus');
+                var summer = summernoteElement;
+                var duplicationYn = false;
+                for(var i = 0; i < summer.find(".mentionUser").length; i++){
+                    var mention = summer.find(".mentionUser")[i];
+                    var id = mention.getAttribute('id');
+                    if(id == scope.userId) duplicationYn = true;
                 }
-                else {
-                    editorElementBody.append('<span class="mentionUser" id="'+ scope.userId +'">@'+ scope.userName +'</span>&nbsp;');
-                    editorElementBody.focus()
-                }
-                var e = $.Event("keydown");
-                e.which = 75;
-                e.keyCode = 75;
-                editorElementBody.trigger(e);
+
+                    var el = editable[1];
+                    var range = document.createRange();
+                    var sel = window.getSelection();
+                    var childLength = el.childNodes.length;
+                    if (childLength > 0) {
+                        var lastNode = el.childNodes[childLength - 1];
+                        var lastNodeChildren = lastNode.childNodes.length;
+                        range.setStart(lastNode, lastNodeChildren);
+                        range.collapse(true);
+                        sel.removeAllRanges();
+                        sel.addRange(range);
+                    }
+
+                    editorElementBody.summernote('pasteHTML', '<span class="mentionUser" id="'+ scope.userId +'">@'+ scope.userName +'</span>&nbsp;');
+
+
+
             });
+
         }
     }
 }
