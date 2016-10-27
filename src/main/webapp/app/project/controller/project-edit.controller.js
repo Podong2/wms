a160e689176b88a7a094af2e6e7e6657a0e0912a
whjang <whjang@wisestone.kr>
2016-10-27 20:52:56 +0900

    * 결함수정

--- src/main/webapp/app/project/controller/project-edit.controller.js
+++ src/main/webapp/app/project/controller/project-edit.controller.js
@@ -9,11 +9,9 @@ angular.module('wmsApp')
 projectCtrl.$inject=['$scope', 'Code', '$log', 'toastr', '$state', '$timeout', 'Project', '$rootScope'];
         function projectCtrl($scope, Code, $log, toastr, $state, $timeout, Project, $rootScope) {
             var vm = this;
-            vm.saveProject = saveProject;
             vm.projectAddOpen = false;
 
             vm.codes = Code.query();
-
 
             /* project info */
             vm.project = {
@@ -42,39 +40,6 @@ projectCtrl.$inject=['$scope', 'Code', '$log', 'toastr', '$state', '$timeout', '
                 //toastr.error('프로젝트 목록 불러오기 실패', '프로젝트 목록 불러오기 실패');
             }
             getList();
-
-            function saveProject(){
-                $log.debug("세이브")
-                if(vm.project.name != '') Project.save(vm.project, onSaveSuccess, onSaveError);
-            }
-
-            function onSaveSuccess (result) {
-                $log.debug("프로젝트 생성 결과 : ", result);
-                toastr.success('프로젝트 생성 완료', '프로젝트 생성 완료');
-                $rootScope.$broadcast("projectAddClose");
-                vm.project.name = '';
-                Project.query({}, onSuccess, onError);
-                $state.go("my-project", {id : result.id});
-            }
-
-
-            function onSaveError () {
-            }
-
-
-
-            function userIdPush(userInfo, type){
-
-                var typeIds = new Array();
-
-                angular.forEach(userInfo, function(val){
-                    typeIds.push(val.id);
-                });
-
-                vm.task[type] = typeIds.join(",");
-            }
-
-
         }
 })();
 
